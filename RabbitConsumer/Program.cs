66b95a30a0f5594a94ae519c2e4020777c52c550
mustafa özüdoğru <mustafa.ozudogru@kocsistem.com.tr>
2020-06-21 15:30:41 +0300

    Add project files.

---
+++ RabbitConsumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "practice",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Person person = JsonConvert.DeserializeObject<Person>(message);
+                    Console.WriteLine($" Name: {person.Name} Surname: {person.SurName} [{person.Message}]");
+                };
+                channel.BasicConsume(queue: "practice",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine("Message Received. Thank you :)");
+                Console.ReadLine();
+            }
+        }
+        public class Person
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string SurName { get; set; }
+            public DateTime BirthDate { get; set; }
+            public string Message { get; set; }
+        }
+    }
+}
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person() { Name = "Mustafa", SurName = "Özüdoğru", ID = 1, BirthDate = new DateTime(1989, 2, 16), Message = "Sample message" };
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "practice",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = JsonConvert.SerializeObject(person);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "practice",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($"Sent Person: {person.Name}-{person.SurName}");
+            }
+
+            Console.WriteLine(" Contact has been sent...");
+            Console.ReadLine();
+        }
+
+        public class Person
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string SurName { get; set; }
+            public DateTime BirthDate { get; set; }
+            public string Message { get; set; }
+        }
+    }
+}
